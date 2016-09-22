9109e6911b0e517409055c590e4dc74f030274fe
Deian Stefan <deian@cs.stanford.edu>
2016-09-21 23:24:49 -0700

    use git diff in rewrite

--- rewrite.sh
+++ rewrite.sh
@@ -1,3 +1,4 @@
-for file in $(git log --name-only --pretty=oneline --full-index HEAD^^..HEAD public | grep -vE '^[0-9a-f]{40} ' | sort | uniq); do
+#for file in $(git log --name-only --pretty=oneline --full-index HEAD^^..HEAD public | grep -vE '^[0-9a-f]{40} ' | sort | uniq); do
+for file in $(git diff --name-only --pretty=oneline public); do
   sed -i "s#cse291.programming.systems#cseweb.ucsd.edu/~dstefan/cse291-fall16#" $file  &
 done
